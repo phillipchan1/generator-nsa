2c53a7abdd15a0ac9bbf22b9c1e538a7d651191d
Phillip_Chan <phillip.chan@liferay.COM>
2017-08-25 14:22:32 -0500

    SF

--- generators/app/templates/server/index.js
+++ generators/app/templates/server/index.js
@@ -14,10 +14,10 @@ app.use(parser.urlencoded({ extended: false }));
 app.use('/', express.static('client/'));
 
 // direct all other routes to client-side app
-app.all('/*', function ( req, res ) {
+app.all('/*', function (req, res) {
     res
         .status( 200 )
-        .set( { 'content-type': 'text/html; charset=utf-8' } )
+        .set({'content-type': 'text/html; charset=utf-8'})
         .sendFile(process.cwd() + '/client/index.html');
 });
 
